ed8b167b1b58c9f1c1506b2cb3491c172e6e97ff
charlottedibb <charlotte.dibb@gmail.com>
2021-06-30 15:39:44 -0700

    Create route to return all feedings

--- server/index.js
+++ server/index.js
@@ -6,4 +6,22 @@ const apiPort = 3000;
 
 app.get("/", (req, res) => res.send("hi"));
 
+app.get("/feedings", async (req, res, next) => {
+  try {
+    const feedings = await knex
+      .from("feedings")
+      .select(
+        "duck_count",
+        "food_type",
+        "food_amount",
+        "latitude",
+        "longitude"
+      );
+    res.json(feedings);
+  } catch (error) {
+    // pass error to default handler
+    return next(error);
+  }
+});
+
 app.listen(apiPort, () => console.log(`api listening on port ${apiPort}`));
