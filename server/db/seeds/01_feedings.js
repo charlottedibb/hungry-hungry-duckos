962d8b45d3bed7f56fe42df02a9d50152d863288
charlottedibb <charlotte.dibb@gmail.com>
2021-07-02 12:15:02 -0700

    Update seeds to include feeding time

--- server/db/seeds/01_feedings.js
+++ server/db/seeds/01_feedings.js
@@ -10,21 +10,24 @@ exports.seed = function(knex) {
           food_type: "bread",
           food_amount: "100g",
           latitude: 0,
-          longitude: 0
+          longitude: 0,
+          feeding_time: "2021-03-01T12:10"
         },
         {
           duck_count: 5,
           food_type: "seeds",
           food_amount: "200g",
           latitude: 0,
-          longitude: 0
+          longitude: 0,
+          feeding_time: "2021-01-01T12:10"
         },
         {
           duck_count: 12,
           food_type: "crackers",
           food_amount: "700g",
           latitude: 0,
-          longitude: 0
+          longitude: 0,
+          feeding_time: "2021-02-01T12:00"
         }
       ]);
     });
