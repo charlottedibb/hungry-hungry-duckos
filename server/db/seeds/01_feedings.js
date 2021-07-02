3af5b8d9c033ebecb12637646c52901d4be4a0f2
charlottedibb <charlotte.dibb@gmail.com>
2021-07-02 12:17:24 -0700

    Add lat/long values to seeds

--- server/db/seeds/01_feedings.js
+++ server/db/seeds/01_feedings.js
@@ -9,24 +9,24 @@ exports.seed = function(knex) {
           duck_count: 1,
           food_type: "bread",
           food_amount: "100g",
-          latitude: 0,
-          longitude: 0,
+          latitude: 48.413232389247604,
+          longitude: -123.36283459495435,
           feeding_time: "2021-03-01T12:10"
         },
         {
           duck_count: 5,
           food_type: "seeds",
           food_amount: "200g",
-          latitude: 0,
-          longitude: 0,
+          latitude: 48.412272013379976,
+          longitude: -123.34595441395885,
           feeding_time: "2021-01-01T12:10"
         },
         {
           duck_count: 12,
           food_type: "crackers",
           food_amount: "700g",
-          latitude: 0,
-          longitude: 0,
+          latitude: 48.41214396189377,
+          longitude: -123.32039299702282,
           feeding_time: "2021-02-01T12:00"
         }
       ]);
