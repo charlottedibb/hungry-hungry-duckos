d6990975d96f3d775e6bbacd78d17860f4142b62
charlottedibb <charlotte.dibb@gmail.com>
2021-07-02 10:54:01 -0700

    Get all feedings in View component

--- client/src/View.js
+++ client/src/View.js
@@ -1,3 +1,15 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 export default function View() {
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/feedings`)
+      .then(response => console.log(response))
+      .catch(error => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
   return <div>View</div>;
 }
