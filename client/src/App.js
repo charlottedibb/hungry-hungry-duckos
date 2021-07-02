ad0e725152dd44be63304ad61891ce6c6151483c
charlottedibb <charlotte.dibb@gmail.com>
2021-07-02 08:34:03 -0700

    Setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Submit from "./Submit";
+import View from "./View";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/submit">Submit</Link>
+            </li>
+            <li>
+              <Link to="/view">View</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/submit">
+            <Submit />
+          </Route>
+          <Route path="/view">
+            <View />
+          </Route>
+          <Route path="/">
+            <h1>Welcome</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
