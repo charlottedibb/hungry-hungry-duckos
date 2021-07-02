1ca8abf0c0187fc7333e9f5bc6015b7eb79521fe
charlottedibb <charlotte.dibb@gmail.com>
2021-07-02 11:08:45 -0700

    Display feedings as list of cards

--- client/src/View.js
+++ client/src/View.js
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Card } from "semantic-ui-react";
 
 export default function View() {
+  const [feedings, setFeedings] = useState([]);
+
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/feedings`)
-      .then(response => console.log(response))
+      .then(res => setFeedings(res.data))
       .catch(error => {
         console.error("There was an error!", error);
       });
   }, []);
 
-  return <div>View</div>;
+  const cards = feedings.map(feeding => (
+    <Card
+      header={`${feeding.duck_count} ducks were feed`}
+      meta={`Latitude: ${feeding.latitude}`}
+      meta={`Longitude: ${feeding.longitude}`}
+      meta={feeding.feeding_time}
+      description={`Type of food: ${feeding.food_type}`}
+      description={`Amount of food: ${feeding.food_amount}`}
+    />
+  ));
+
+  return <div>{cards}</div>;
 }
